a8db2526fdf8761d394b60811196eac6223e5111
PrinceHumbug <huttner.evan@gmail.com>
2024-11-05 14:41:40 -0600

    D: launch V1.0

--- main.py
+++ main.py
@@ -3,13 +3,35 @@ def main():
     text = get_book_text(book_path)
     num_words = count_words(text)
     num_characters = count_characters_dict(text)
-    print(f"{num_words} words found in the document, with a total of {num_characters} characters.")
+    character_sorted_list = char_dict_to_sorted_list(num_characters)
+
+    print (f"--- Begin report of {book_path}---")
+    print(f"{num_words} found in the document")
+    print()
+
+    for item in character_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item[ 'num']} times")
+
+    print("--- End report ---")
+
 
     
 
 def count_words(words):
     word_count = words.split()
     return len(word_count) 
+
+def sort_on(d):
+   return d["num"]
+
+def char_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 def count_characters_dict(text):
     characters = {}
