d2780a442a246b62dc0d3b9756c3cdb418c26b3d
PrinceHumbug <huttner.evan@gmail.com>
2024-11-05 14:04:11 -0600

    B: word count

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 def main():
     book_path = "/home/evanh/workspace/github.com/PrinceHumbug/bookbot/books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    num_words = count_words(text)
+    print(f"{num_words} words found in the document")
+
+    
+
+def count_words(words):
+    word_count = words.split()
+    return len(word_count) 
 
 def get_book_text(path):   
     with open(path) as f:
         return f.read()
+
+
 
 
 main()
