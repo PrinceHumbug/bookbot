2c1bc02e8a2a56d4d58a422df5f8ae5a1560cdcd
PrinceHumbug <huttner.evan@gmail.com>
2024-11-05 14:21:08 -0600

    C: character count

--- main.py
+++ main.py
@@ -2,13 +2,24 @@ def main():
     book_path = "/home/evanh/workspace/github.com/PrinceHumbug/bookbot/books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = count_words(text)
-    print(f"{num_words} words found in the document")
+    num_characters = count_characters_dict(text)
+    print(f"{num_words} words found in the document, with a total of {num_characters} characters.")
 
     
 
 def count_words(words):
     word_count = words.split()
     return len(word_count) 
+
+def count_characters_dict(text):
+    characters = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in characters:
+            characters[lowered] += 1
+        else:
+            characters[lowered] = 1
+    return characters
 
 def get_book_text(path):   
     with open(path) as f:
